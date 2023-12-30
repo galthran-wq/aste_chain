c23c936149d66e60bb2d5095be958722be579ebf
galthran-wq <levmorozov900@gmail.com>
2023-12-30 17:27:29 +0300

    run chain util

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from tqdm.auto import tqdm
 from pathlib import Path
 from langchain.chat_models.gigachat import GigaChat
 
@@ -20,3 +21,25 @@ def setup_gigachat(env_file_path="./.env"):
         **env
     )
     return chat
+
+
+    from tqdm.auto import tqdm
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def run_chain(texts, chain, max_workers=2, batch_size=32):
+    result = []
+    batch_size = 32
+    max_workers = 2
+    for batch in tqdm(chunks(texts, batch_size), total=len(texts) // batch_size):
+        try:
+            result.extend(
+                chain.batch(batch, config={"max_concurrency": max_workers})
+            )
+        except Exception as e:
+            result.extend([ None ] * batch_size)
+    return result
