550055a85d2dca32a1b110a423d837c57df0767b
galthran-wq <levmorozov900@gmail.com>
2023-12-31 15:45:30 +0300

    embeddings fix

--- embeddings.py
+++ embeddings.py
@@ -6,6 +6,6 @@ class E5HuggingfaceEmbeddings(HuggingFaceEmbeddings):
         texts = [ f"passsage: {text}" for text in texts]
         return super().embed_documents(texts)
 
-    def embed_query(self, texts: List[str]) -> List[List[float]]:
-        texts = [ f"query: {text}" for text in texts]
-        return super().embed_documents(texts)
+    def embed_query(self, query: str, passage=True) -> List[float]:
+        query = f"{'query:' if not passage else 'passage:'} {query}"
+        return super().embed_query(query)
